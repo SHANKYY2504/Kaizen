21524ee929795353b0e0bcd8557f83cae0b932d5
Shashank Jha <shashankjha@Shashanks-MacBook-Air.local>
2026-02-05 14:51:08 +0530

    feat: implement initial landing page with 3D hero section

---
+++ app/layout.js
@@ -0,0 +1,54 @@
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider"
+import { dark} from '@clerk/themes'
+
+
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({
+  subsets: ["latin"]
+});
+
+
+export const metadata = {
+  title: "Kaizen",
+  description: "for preparation",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider  
+        appearance={{
+        theme: dark,
+      }}>
+      
+      <html lang="en">
+      <body
+        className={`${inter.className} `}
+        >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+            >
+          {/* <header /> */}
+          <Header />
+        
+          <main className="min-h-screen">
+            {children}
+          </main>
+          {/* <footer/> */}
+          <footer className="bg-muted/50 py-12">
+            <div className=" container mx:auto px-4 text-center text-gray-200">
+              made by shashank jha
+            </div>
+          </footer>
+          </ThemeProvider>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
